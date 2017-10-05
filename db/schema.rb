acace71af90537db6e3a2226e059c1747efb2aa2
erikzaksf <erik.zak.sf@gmail.com>
2017-10-05 11:02:53 -0700

    change column name fix spelling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005155051) do
+ActiveRecord::Schema.define(version: 20171005175948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20171005155051) do
   create_table "meetups", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "catagory"
+    t.string "category"
     t.string "street"
     t.string "city"
     t.string "state"
